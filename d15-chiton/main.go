14e546c2f63eca030bd78fc91c0b621e7f4b2161
Alextopher <christopher.mahoney2000@gmail.com>
2022-05-11 19:18:26 -0400

    remove code duplication

--- d15-chiton/main.go
+++ d15-chiton/main.go
@@ -7,49 +7,11 @@ import (
 	"os"
 )
 
-func part1(filename string) {
-	// Read the input
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	size := len(scanner.Text())
-	file.Close()
+type position struct {
+	x, y int
+}
 
-	file, err = os.Open(filename)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	scanner = bufio.NewScanner(file)
-	defer file.Close()
-
-	// Create the grid
-	grid := make([][]byte, size)
-	for i := 0; i < size; i++ {
-		grid[i] = make([]byte, size)
-	}
-
-	// Fill the grid with the input
-	x, y := 0, 0
-	for scanner.Scan() {
-		line := scanner.Text()
-		for _, c := range line {
-			grid[y][x] = byte(c) - '0'
-			x++
-		}
-		x = 0
-		y++
-	}
-
-	// Dijkstra's algorithm is the best way to do this
-	type position struct {
-		x, y int
-	}
-
+func dijkstra(grid [][]byte, start position, end position, size int) int {
 	queue := PriorityQueue{}
 
 	// create distance and previous grids
@@ -110,49 +72,14 @@ func part1(filename string) {
 		}
 	}
 
-	// print the distance to the bottom right corner
-	fmt.Println(dp[size-1][size-1].dist)
+	return dp[size-1][size-1].dist
 }
 
-func part2(filename string) {
-	// basically the same as part 1, but we need to massively enlarge the grid
-	// Read the input
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	scanner := bufio.NewScanner(file)
-	scanner.Scan()
-	size := len(scanner.Text())
-	file.Close()
+func part1(grid [][]byte, size int) int {
+	return dijkstra(grid, position{0, 0}, position{size - 1, size - 1}, size)
+}
 
-	file, err = os.Open(filename)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	scanner = bufio.NewScanner(file)
-	defer file.Close()
-
-	// Create the grid
-	grid := make([][]byte, size)
-	for i := 0; i < size; i++ {
-		grid[i] = make([]byte, size)
-	}
-
-	// Fill the grid with the input
-	x, y := 0, 0
-	for scanner.Scan() {
-		line := scanner.Text()
-		for _, c := range line {
-			grid[y][x] = byte(c) - '0'
-			x++
-		}
-		x = 0
-		y++
-	}
-
+func part2(grid [][]byte, size int) int {
 	// Create the larger grid
 	larger := make([][]byte, size*5)
 	for i := 0; i < size*5; i++ {
@@ -178,79 +105,47 @@ func part2(filename string) {
 		}
 	}
 
-	size = 5 * size
-	grid = larger
-
-	// Dijkstra's algorithm is the best way to do this
-	type position struct {
-		x, y int
-	}
-
-	queue := PriorityQueue{}
-
-	// create distance and previous grids
-	type distprev struct {
-		dist int
-		prev position
-	}
-
-	dp := make([][]distprev, size)
-	for i := 0; i < size; i++ {
-		dp[i] = make([]distprev, size)
-		for j := 0; j < size; j++ {
-			if i == 0 && j == 0 {
-				dp[i][j].dist = 0
-			} else {
-				// initialize distance to infinity
-				dp[i][j].dist = math.MaxInt32
-			}
-
-			// initialize previous to nil
-			dp[i][j].prev = position{-1, -1}
-
-			queue.Push(&Item{value: position{i, j}, priority: dp[i][j].dist})
-		}
-	}
-
-	// while queue is not empty
-	for queue.Len() > 0 {
-		// get the next item
-		item := queue.Pop().(*Item)
-
-		// fmt.Println("Item:", item.value, item.priority)
-
-		// calculate neighbors
-		neighbors := make([]position, 0)
-		if item.value.x > 0 {
-			neighbors = append(neighbors, position{item.value.x - 1, item.value.y})
-		}
-		if item.value.x < size-1 {
-			neighbors = append(neighbors, position{item.value.x + 1, item.value.y})
-		}
-		if item.value.y > 0 {
-			neighbors = append(neighbors, position{item.value.x, item.value.y - 1})
-		}
-		if item.value.y < size-1 {
-			neighbors = append(neighbors, position{item.value.x, item.value.y + 1})
-		}
-
-		// for each neighbor
-		for _, neighbor := range neighbors {
-			alt := dp[item.value.y][item.value.x].dist + int(grid[neighbor.y][neighbor.x])
-			if alt < dp[neighbor.y][neighbor.x].dist {
-				dp[neighbor.y][neighbor.x].dist = alt
-				dp[neighbor.y][neighbor.x].prev = item.value
-
-				queue.Push(&Item{value: neighbor, priority: alt})
-			}
-		}
-	}
-
-	// print the distance to the bottom right corner
-	fmt.Println(dp[size-1][size-1].dist)
+	return dijkstra(larger, position{0, 0}, position{size - 1, size - 1}, 5*size)
 }
 
 func main() {
-	part1("input.txt")
-	part2("input.txt")
+	// Read the input
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+	size := len(scanner.Text())
+	file.Close()
+
+	file, err = os.Open("input.txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	scanner = bufio.NewScanner(file)
+	defer file.Close()
+
+	// Create the grid
+	grid := make([][]byte, size)
+	for i := 0; i < size; i++ {
+		grid[i] = make([]byte, size)
+	}
+
+	// Fill the grid with the input
+	x, y := 0, 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		for _, c := range line {
+			grid[y][x] = byte(c) - '0'
+			x++
+		}
+		x = 0
+		y++
+	}
+
+	fmt.Println(part1(grid, size))
+	fmt.Println(part2(grid, size))
 }
