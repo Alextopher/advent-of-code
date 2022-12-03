9929e4ce07503f64169c409bd96c55da68716b24
Alextopher <christopher.mahoney2000@gmail.com>
2022-12-03 01:05:30 -0500

    sufficently efficient with bit masks

--- d03/src/main.rs
+++ d03/src/main.rs
@@ -1,47 +1,43 @@
 use aoc::get_lines;
 use itertools::Itertools;
 
-fn main() {
-    let ans: i32 = get_lines("input.txt")
-        .map(|s| {
-            let chars: Vec<char> = s.chars().collect();
-            let len = chars.len();
+fn get_mask(s: &str) -> u64 {
+    let mut mask = 0;
 
-            let first_half: String = chars.iter().take(len / 2).collect();
-            let second_half: String = chars.iter().skip(len / 2).collect();
-
-            (first_half, second_half)
-        })
-        .map(|(l, r)| {
-            for c in l.chars() {
-                if r.contains(c) {
-                    if c.is_uppercase() {
-                        return c as i32 - 'A' as i32 + 27;
-                    } else {
-                        return c as i32 - 'a' as i32 + 1;
-                    }
-                }
+    s.chars()
+        .map(|c| {
+            if c.is_ascii_uppercase() {
+                c as i32 - 'A' as i32 + 27
+            } else {
+                c as i32 - 'a' as i32 + 1
             }
-            0
         })
+        .for_each(|b| mask |= 1 << b);
+
+    mask
+}
+
+fn main() {
+    let ans: u32 = get_lines("input.txt")
+        .map(|s| {
+            let split = s.split_at(s.len() / 2);
+            get_mask(split.0) & get_mask(split.1)
+        })
+        .map(u64::trailing_zeros)
         .sum();
 
     println!("{ans}");
 
-    let ans: i32 = get_lines("input.txt").chunks(3).into_iter().map(|k| {
-        let groups : Vec<String> = k.collect();
-        // find element in g[0] that is in g[1] and g[2]
-        for c in groups[0].chars() {
-            if groups[1].contains(c) && groups[2].contains(c) {
-                    if c.is_uppercase() {
-                        return c as i32 - 'A' as i32 + 27;
-                    } else {
-                        return c as i32 - 'a' as i32 + 1;
-                    }
-            }
-        }
-        0
-    }).sum();
+    let ans: u32 = get_lines("input.txt")
+        .chunks(3)
+        .into_iter()
+        .map(|k| {
+            let mut mask = u64::MAX;
+            k.for_each(|s| mask &= get_mask(&s));
+            mask
+        })
+        .map(u64::trailing_zeros)
+        .sum();
 
     println!("{ans}");
 }
