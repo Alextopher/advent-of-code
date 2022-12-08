34388b63b037a4d7bf66a96729f5afb97b6a12c2
Alextopher <christopher.mahoney2000@gmail.com>
2022-12-08 12:16:42 -0500

    extract tree data type from day 7

--- aoc/src/lib.rs
+++ aoc/src/lib.rs
@@ -1,19 +1,21 @@
 pub mod get_mut;
 pub mod iterstuff;
+pub mod tree;
 pub mod unionfind;
 
 pub use get_mut::GetMultMut;
 pub use iterstuff::IterJunk;
+pub use tree::{Node, Tree};
 pub use unionfind::UnionFind;
 
-use std::{
-    fs::File,
-    io::{BufRead, BufReader},
-    path::Path,
-};
+use std::{fs, path::Path};
 
+/// Read the entire file into memory as a string.
+/// Then split it on newlines.
 pub fn get_lines(f: impl AsRef<Path>) -> impl Iterator<Item = String> {
-    let file = File::open(f).unwrap();
-    let reader = BufReader::new(file);
-    reader.lines().map(Result::unwrap)
+    let file = fs::read_to_string(f).unwrap();
+    file.lines()
+        .map(|l| l.to_owned())
+        .collect::<Vec<_>>()
+        .into_iter()
 }
--- d07/src/main.rs
+++ d07/src/main.rs
@@ -1,174 +1,111 @@
-use std::{
-    cell::RefCell,
-    rc::{Rc, Weak},
-};
+use std::{cell::RefCell, time::Instant};
 
-use aoc::*;
+use aoc::{Node, Tree};
 use itertools::Itertools;
 
-// A tree to store something that looks like a file system
-#[derive(Debug)]
-enum NodeInner {
-    File(u32),
-    Directory(RefCell<Vec<Rc<Node>>>),
-}
-
-impl NodeInner {
-    fn is_directory(&self) -> bool {
-        matches!(self, NodeInner::Directory(_))
-    }
-
-    fn as_directory(&self) -> Option<&RefCell<Vec<Rc<Node>>>> {
-        match &self {
-            NodeInner::File(_) => None,
-            NodeInner::Directory(children) => Some(children),
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Node {
-    inner: NodeInner,
-    name: String,
-    parent: RefCell<Weak<Node>>,
-}
-
-impl Node {
-    fn size(&self) -> u32 {
-        match &self.inner {
-            NodeInner::File(size) => *size,
-            NodeInner::Directory(children) => children
-                .borrow()
-                .iter()
-                .map(|child| child.size())
-                .sum::<u32>(),
-        }
-    }
-
-    /// Returns an iterator over _all_ directories in the tree
-    fn all_directories(&self) -> Vec<Rc<Node>> {
-        let mut stack: Vec<_> = self
-            .inner
-            .as_directory()
-            .unwrap()
-            .borrow()
-            .iter()
-            .filter(|child| child.inner.is_directory())
-            .cloned()
-            .collect();
-
-        let mut directories = vec![];
-
-        while let Some(node) = stack.pop() {
-            directories.push(node.clone());
-
-            if let Some(children) = node.inner.as_directory() {
-                let c = children.borrow();
-                stack.extend(
-                    c.iter()
-                        .filter(|dir_entry| dir_entry.inner.is_directory())
-                        .cloned(),
-                );
-            }
-        }
-
-        directories
-    }
-}
-
 fn main() {
-    let file_system = Rc::new(Node {
-        inner: NodeInner::Directory(RefCell::new(Vec::new())),
-        name: String::from("/"),
-        parent: RefCell::new(Weak::new()),
-    });
+    let mut time = Instant::now();
+    let mut lines = aoc::get_lines("input.txt").peekable();
 
-    let mut current_dir = Rc::clone(&file_system);
+    // Don't count the time it takes to read the input into memory
+    println!("Read input {:?}", time.elapsed());
+    time = Instant::now();
 
-    // each line is either "$ cd <path>" or "$ ls" or "<size> filename"
-    // we need to read this into a tree
-    for line in get_lines("input.txt") {
-        // split input
-        let parts = line.split_whitespace().collect_vec();
+    let file_system: Tree<RefCell<(String, Option<usize>)>> =
+        Tree::new(RefCell::new((String::from("/"), None)));
+    let mut current_node = file_system.root();
 
-        if parts[0] == "$" {
-            if parts[1] == "cd" {
-                if parts[2] == ".." {
-                    // go up a directory
-                    current_dir = current_dir
-                        .clone()
-                        .parent
-                        .borrow()
-                        .upgrade()
-                        .expect("no parent directory");
-                } else if parts[2] == "/" {
-                    current_dir = Rc::clone(&file_system);
-                } else {
-                    // go down a directory
-                    let new_dir = current_dir
-                        .inner
-                        .as_directory()
-                        .expect("tried to go down from file")
-                        .borrow()
-                        .iter()
-                        .find(|child| child.name == parts[2])
-                        .expect("tried to go down to non-existent directory")
-                        .clone();
+    while let Some(line) = lines.next() {
+        let split = line.split_whitespace().collect_vec();
 
-                    current_dir = new_dir;
+        // Either "$ cd path" or "$ ls"
+        if split[1] == "cd" {
+            let path = split[2];
+
+            if path == ".." {
+                current_node = current_node.parent().unwrap();
+            } else if path == "/" {
+                current_node = file_system.root();
+            } else {
+                current_node = current_node
+                    .children()
+                    .into_iter()
+                    .find(|n| (*n).borrow().0 == path)
+                    .unwrap();
+            }
+        } else if split[1] == "ls" {
+            // next few lines are "dir name" or "size name"
+            while let Some(line) = lines.peek() {
+                let split = line.split_whitespace().collect_vec();
+                // If the next line is a command we're done processing this ls
+                if split[0] == "$" {
+                    break;
                 }
-            } else if parts[1] == "ls" {
-                // do nothing
+
+                let name = split[1];
+                if split[0] == "dir" {
+                    current_node.add_child(RefCell::new((name.to_string(), None)));
+                } else {
+                    let size = split[0].parse::<usize>().unwrap();
+                    let mut inner = current_node.borrow_mut();
+
+                    match inner.1 {
+                        Some(ref mut s) => *s += size,
+                        None => inner.1 = Some(size),
+                    }
+                }
+
+                lines.next();
             }
         } else {
-            // add file to current directory
-            if parts[0] == "dir" {
-                current_dir
-                    .inner
-                    .as_directory()
-                    .unwrap()
-                    .borrow_mut()
-                    .push(Rc::new(Node {
-                        inner: NodeInner::Directory(RefCell::new(Vec::new())),
-                        name: parts[1].to_string(),
-                        parent: RefCell::new(Rc::downgrade(&current_dir)),
-                    }));
-            } else {
-                let size = parts[0].parse::<u32>().expect("invalid size");
-
-                current_dir
-                    .inner
-                    .as_directory()
-                    .unwrap()
-                    .borrow_mut()
-                    .push(Rc::new(Node {
-                        inner: NodeInner::File(size),
-                        name: parts[1].to_string(),
-                        parent: RefCell::new(Rc::downgrade(&current_dir)),
-                    }));
-            }
+            panic!("Unknown command: {}", split[1]);
         }
     }
+    println!("parse: {:?}", time.elapsed());
+    time = Instant::now();
 
-    let sizes = file_system
-        .all_directories()
-        .into_iter()
-        .map(|dir| dir.size())
-        .filter(|size| *size < 100000)
-        .sum::<u32>();
+    update_sizes(&mut file_system.root());
+    println!("update_sizes: {:?}", time.elapsed());
+    time = Instant::now();
 
-    println!("total size: {}", sizes);
+    // Part 1 - sum the sizes of directories < 100000
+    let sum = file_system
+        .iter_preorder()
+        .filter_map(|n| {
+            let size = n.borrow().1.unwrap();
+            (size < 100000).then_some(size)
+        })
+        .sum::<usize>();
+
+    println!("{} {:?}", sum, time.elapsed());
 
     let max_size = 70000000;
-    let remaining = max_size - file_system.size();
+    let remaining = max_size - file_system.root().borrow().1.unwrap();
 
     // find the smallest file such that total size - file_system > 30000000
     let smallest_file_size = file_system
-        .all_directories()
-        .into_iter()
-        .map(|dir| dir.size())
+        .iter_preorder()
+        .map(|n| n.borrow().1.unwrap())
         .filter(|size| remaining + size > 30000000)
-        .min();
+        .min()
+        .unwrap();
 
-    println!("smallest file: {}", smallest_file_size.unwrap());
+    println!("{} {:?}", smallest_file_size, time.elapsed());
+}
+
+/// Update the sizes of all the directories to include the sizes of their children
+/// + their own size
+fn update_sizes(node: &mut Node<RefCell<(String, Option<usize>)>>) {
+    let mut size = 0;
+
+    for mut child in node.children() {
+        update_sizes(&mut child);
+        size += child.borrow().1.unwrap();
+    }
+
+    let mut inner = node.borrow_mut();
+    match inner.1 {
+        Some(ref mut s) => *s += size,
+        None => inner.1 = Some(size),
+    }
 }
